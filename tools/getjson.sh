95b2902c0515160952c07b410e8a29186e6bfa8d
Trevor Smigiel <tsmigiel@yahoo.com>
2020-01-27 16:59:54 -0600

    Update with latest data from FFG API
    This includes new points and new pilots and upgrades.
    
    Updated by simply running tools/getjson.sh

--- tools/getjson.sh
+++ tools/getjson.sh
@@ -13,8 +13,8 @@ done
 
 echo "Fetching json and storing to $data_dir"
 
-curl https://squadbuilder.fantasyflightgames.com/api/cards/pilots/ | json_reformat > "$data_dir/pilots.json"
-curl https://squadbuilder.fantasyflightgames.com/api/cards/upgrades/ | json_reformat > "$data_dir/upgrades.json"
+curl https://squadbuilder.fantasyflightgames.com/api/cards/pilots/ | node sortcards.js | json_reformat > "$data_dir/pilots.json"
+curl https://squadbuilder.fantasyflightgames.com/api/cards/upgrades/ | node sortcards.js | json_reformat > "$data_dir/upgrades.json"
 curl https://squadbuilder.fantasyflightgames.com/api/app-metadata/ | json_reformat > "$data_dir/app-metadata.json"
 curl https://squadbuilder.fantasyflightgames.com/api/cards/extensions/ | json_reformat > "$data_dir/cards_extensions.json"
 curl https://squadbuilder.fantasyflightgames.com/api/gameformats/ | json_reformat > "$data_dir/gameformats.json"
