a4de32ee56601644b0f4b42ab69fda4b65b14e98
Trevor Smigiel <tsmigiel@yahoo.com>
2020-03-26 18:20:06 +0900

    Remove some of the "id" fields
    These id fields in pilot and upgrade cards are updated every time
    the data gets updated. This app doesn't need them so delete them
    so we can see smaller diffs after every update.

--- tools/sortcards.js
+++ tools/sortcards.js
@@ -3,5 +3,9 @@ var fs = require('fs');
 var data = fs.readFileSync(0, 'utf-8');
 var json = JSON.parse(data)
 json.cards.sort((a, b) => parseInt(a.id) - parseInt(b.id))
+json.cards.forEach((card) => {
+	card.available_actions.forEach((a) => delete a.id)
+	card.statistics.forEach((a) => delete a.id)
+})
 process.stdout.write(JSON.stringify(json))
 
